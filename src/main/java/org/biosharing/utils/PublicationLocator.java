4987a3d83eab8f7e7f5f4f546f5010fdb8cfaf24
Eamonn Maguire <eamonnmag@gmail.com>
2012-05-22 11:11:25 +0100

    Initial commit of biosharing utility code.

---
+++ src/main/java/org/biosharing/utils/PublicationLocator.java
@@ -0,0 +1,12 @@
+package org.biosharing.utils;
+
+/**
+ * Created by the ISA team
+ *
+ * @author Eamonn Maguire (eamonnmag@gmail.com)
+ *         <p/>
+ *         Date: 22/05/2012
+ *         Time: 11:08
+ */
+public class PublicationLocator {
+}
